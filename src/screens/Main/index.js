c19eb13b22a8c84328b9782ace1d9b80f4749a5b
andrefangeloni <andrefangeloni@gmail.com>
2020-02-06 23:04:53 -0300

    Setup enviroment

---
+++ src/screens/Main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import styles from './styles';
+
+const Main = () => {
+  return (
+    <View>
+      <Text>Main</Text>
+    </View>
+  );
+};
+
+export default Main;
---
+++ src/screens/Profile/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import styles from './styles';
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  );
+};
+
+export default Profile;
